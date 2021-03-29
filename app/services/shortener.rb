74ed53e4906c4d6c90604e272025a0b4f7b70e58
Abubakar Diallo <dialloabubakar5861@gmail.com>
2021-03-29 14:04:23 +0100

    link model generated and services created for generating short url and slugs

---
+++ app/services/shortener.rb
@@ -0,0 +1,30 @@
+class Shortener
+  attr_reader :url, :link_model
+
+  def initialize(url, link_model = Link)
+    @url = url
+    @link_model = link_model
+  end
+
+  def generate_short_link
+    link_model.create(original_url: url, lookup_code: lookup_code)
+  end
+
+  def lookup_code
+    # 1. get a lookup code
+    # 2. check if it already exists
+    # 3. break out of the loop if it does not exists
+    # 4. if not, get another code
+
+    loop do
+      code = get_fresh_code
+      break code unless link_model.exists?(lookup_code: code)
+    end
+  end
+
+  private
+
+  def get_fresh_code
+    SecureRandom.uuid[0..6]
+  end
+end
