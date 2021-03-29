847be17f5b89fcc74a5e8ee4ec95df4737f415fb
Abubakar Diallo <dialloabubakar5861@gmail.com>
2021-03-29 14:47:17 +0100

    method added for validating original url and method for the shorten url

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,15 @@
 class Link < ApplicationRecord
   validates_presence_of :lookup_code, :original_url
   validates_uniqueness_of :lookup_code
+  validate :original_url_format
+
+  def original_url_format
+    uri = URI.parse(original_url || '')
+
+    errors.add(:original_url, 'Invalid URL format') if uri.host.nil?
+  end
+
+  def shortened_url
+    "http://localhost:3000/#{lookup_code}"
+  end
 end
